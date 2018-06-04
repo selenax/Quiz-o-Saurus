ba3a2d7016e9526e7eafc958202865da786bf7a6
yona <04l.yona@gmail.com>
2018-06-04 16:34:15 -0400

    set cookie when client logs in, delete cookie when client logs out

--- server/index.js
+++ server/index.js
@@ -2,17 +2,26 @@
 const express = require('express');
 const bodyParser = require('body-parser'); //parses incoming req
 const data = require('../database-mongo');
+const app = express();
 
-const app = express(),
-      passport = require('passport'),
+//import google oauth passport package
+const passport = require('passport'),
       auth = require('./auth.js');
 
-auth(passport);
+//import cookie package
+const cookieParser = require('cookie-parser'),
+    cookieSession = require('cookie-session');
+
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+auth(passport);
 app.use(passport.initialize());
+//keep key secret so cookie don't get stoled by 3rd parties
+app.use(cookieSession({name: 'session', keys: ['810']}));
+app.use(cookieParser());
 
 
 //google api console get req
@@ -20,23 +29,42 @@ app.use(passport.initialize());
   //callback: http://localhost:3000/auth/google/callback
   //client secret: LfaK1hn8P-3KDjsIAdv9tWQf
   //then do post req when user confirms google authentication
+
+//redirects client to google login page
 app.get('/', (req, res) => {
+  if (req.session.token) { //if token exists
+    res.cookie('token', req.session.token); //set cookie
+    res.json({
+        status: 'session cookie set'
+    });
+  } else {
+    res.cookie('token', '') //cookie is not set
     res.json({
         status: 'session cookie not set'
     });
+  }
 });
 
+//redirects client to google login page
 app.get('/auth/google', passport.authenticate('google', {
     scope: ['https://www.googleapis.com/auth/userinfo.profile']
 }));
 
+//set the cookie session when user is logged in
 app.get('/auth/google/callback',
-    passport.authenticate('google', {
-        failureRedirect: '/'
-    }),
-    (req, res) => {}
+  passport.authenticate('google', {failureRedirect: '/'}),
+  (req, res) => {
+    req.session.token = req.user.token;
+    res.redirect('/'); //takes client to '/'
+  }
 );
 
+//google log out
+app.get('/logout', (req, res) => {
+    req.logout();
+    req.session = null; //delete the set cookie
+    res.redirect('/');
+});
 
 //overall, there are two get requests, note** need list of endpoints
 //get request for user info (includes email, global score, and score for each attempted quiz)
