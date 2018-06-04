7248ca2cd0d94d9d4a11ce7a97173d868f06139c
yona <04l.yona@gmail.com>
2018-06-04 14:49:11 -0400

    get and post request available, not fully implemented because dont have solid endpoint names and database func to save/fetch info, getting started to read google oauth

--- server/index.js
+++ server/index.js
@@ -1,19 +1,52 @@
+//starting up express onto server
 var express = require('express');
-var bodyParser = require('body-parser');
-var items = require('../database-mongo');
+var bodyParser = require('body-parser'); //parses incoming req
+var data = require('../database-mongo');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(bodyParse.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
+
+//google api console get req
+  //then do post req when user confirms google authentication
+
+
+//overall, there are two get requests, note** need list of endpoints
+//get request for user info (includes email, global score, and score for each attempted quiz)
+app.get('/endpoint-for-user-change-l8r', function (req, res) {
+  //fetch info from database (.retrieve name may vary l9r)
+  data.retrieve(function(err, data) {
     if(err) {
       res.sendStatus(500);
     } else {
-      res.json(data);
+      res.send('getting user info...');
     }
   });
+});
+
+//get request for quizzes
+app.get('/endpoint-for-quiz-change-l8r', function (req, res) {
+  //fetch info from database (.retrieve name may vary l9r)
+  data.retrieve(function(err, data) {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.send('fetching quiz...')
+      // res.json(data);
+    }
+  });
+});
+
+//post req to update tally at end of quiz for attempted quiz
+app.post('/endpoint-for-user-change-l8r', function(req, res) {
+  //we have info of username, quiz name, and score
+  var quizComplete = req.body;
+
+  //save info to database (.save name may vary l8r)
+  data.save(quizComplete);
 });
 
 app.listen(3000, function() {
