966e73417664e6e62871caabf311e960575398a1
yona <04l.yona@gmail.com>
2018-06-10 02:59:56 -0400

    remove the creator name and rendering the quiz title name

--- react-client/src/components/Home/QuizListEntry.jsx
+++ react-client/src/components/Home/QuizListEntry.jsx
@@ -8,7 +8,6 @@ const QuizListEntry = (props) => {
   return (
     <li className="quiz-list-display" onClick={() => {clicker(props.quiz)}}>
       <img src={props.quiz.imgUrl} className="quiz-list-image"/>
-      <div className="quiz-list-creator">{props.quiz.creator}</div>
       <div className="quiz-list-title">{props.quiz.quizName}</div>
   </li>
   )
