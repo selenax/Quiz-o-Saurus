9dcef126d8d7330f5ee87bb97c2b8085fff6a4c9
selenax <selenawenxu@gmail.com>
2018-06-11 19:32:17 -0700

    Update gitignore

--- react-client/src/components/Root/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  googleClientID: '693481139065-u6kaus22pakvl1l3t9llh81ndpav3sht.apps.googleusercontent.com',
-  googleClientSecret: '_B3hI6xc-azWxI0Ly8HWOSM5',
-  mongoURI: ""
-};
